f890d3c0264d1b721c0561ccd12b087d234f7360
Peter Cho <pcho231@gmail.com>
2021-07-23 14:01:26 -0400

    SUCCESS

--- __tests__/App.js
+++ __tests__/App.js
@@ -1,10 +1,15 @@
 import 'react-native';
 import React from 'react';
+import { render, fireEvent } from '@testing-library/react-native'
 import App from '../App';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
-  renderer.create(<App />);
+  const { getByText } = render(<App />);
+
+  fireEvent.press(getByText('Go to Details'))
+
+  getByText('Details Screen')
 });
